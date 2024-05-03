aa4775f976fcaabe279b6d90081a7dfb8122427f
Alexander <lime4512@mail.ru>
2024-05-04 00:19:43 +0300

    add  react-hot-toast

--- src/components/AddTask/AddTask.jsx
+++ src/components/AddTask/AddTask.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import styles from './addTask.module.scss'
 import { v4 as uuidv4 } from 'uuid'
-
+import toast, { Toaster } from 'react-hot-toast'
 export const AddTask = ({ tasks, setTasks }) => {
 	const [task, setTask] = useState({
 		id: '',
@@ -11,6 +11,14 @@ export const AddTask = ({ tasks, setTasks }) => {
 
 	const handleSubmit = e => {
 		e.preventDefault()
+
+		if (task.name == 0) {
+			return toast.error('Введите данные!!!')
+		}
+
+		if (task.name.length <= 3) {
+			return toast.error('Более 3 символов')
+		}
 
 		setTasks(prev => {
 			const list = [...prev, task]
@@ -29,6 +37,7 @@ export const AddTask = ({ tasks, setTasks }) => {
 				className={styles.add_input}
 				onChange={e => setTask({ ...task, id: uuidv4(), name: e.target.value })}
 			/>
+			<Toaster position='top-right' reverseOrder={false} />
 			<div className={styles.container_btn}>
 				<button className={styles.add_btn}>
 					<span className='material-symbols-outlined'>add</span>Добавить
